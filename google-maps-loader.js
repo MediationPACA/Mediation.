4f6a679656f32a51d43f64a2212d78a98d459e29
MediationPACA <adribuchet@gmail.com>
2025-03-06 13:45:52 +0100

    Add files via upload

---
+++ google-maps-loader.js
@@ -0,0 +1,7 @@
+const apiKey = "YOUR_API_KEY";  // Ceci sera remplacé par GitHub Actions
+
+const script = document.createElement("script");
+script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
+script.async = true;
+script.defer = true;
+document.head.appendChild(script);
