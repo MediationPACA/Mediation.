49c652dd403d369a45aff4d4dd85576f060e61ba
MediationPACA <adribuchet@gmail.com>
2025-04-08 09:37:13 +0200

    Mise à jour du site avec les derniers changements

--- auth.js
+++ auth.js
@@ -1,19 +1,18 @@
 (function () {
     function checkPassword(input) {
-        // Mot de passe encodé en base64 (pour l'exemple)
         var encodedPassword = "bWV0b3RhY2FwODM=";
         return btoa(input) === encodedPassword;
     }
 
     function getCurrentPassword() {
-        return "bWV0b3RhY2FwODM="; // identique à encodedPassword
+        return "bWV0b3RhY2FwODM="; 
     }
 
     window.verifier = function () {
         var mdp = document.getElementById("password").value;
         if (checkPassword(mdp)) {
-            localStorage.setItem("auth", btoa(mdp)); // Stocke le mot de passe encodé
-            window.location.href = "index.html"; // Redirige vers la page protégée
+            localStorage.setItem("auth", btoa(mdp)); /
+            window.location.href = "index.html"; // 
         } else {
             alert("Mot de passe incorrect !");
         }
